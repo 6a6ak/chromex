2e4aef6b12d964486523c34a6010dcf4c4f7c549
6a6ak <babak.ahari@gmail.com>
2023-07-27 00:58:20 +0200

    2DO 4 Chrome

---
+++ popup.js
@@ -0,0 +1,80 @@
+let todoList = [];
+
+// Save todoList to chrome storage
+function saveTasks() {
+    chrome.storage.sync.set({ todoList: todoList });
+}
+
+// Load tasks from chrome storage
+function loadTasks() {
+    chrome.storage.sync.get('todoList', function(data) {
+        if (data.todoList) {
+            todoList = data.todoList;
+            updateList();
+        }
+    });
+}
+
+// Delete tasks from chrome storage
+function deleteTask(index) {
+    todoList.splice(index, 1); // Remove the task at this index
+    saveTasks(); // Save the updated task list
+    updateList(); // Refresh the task list display
+}
+
+
+function addTask() {
+    let taskText = document.getElementById('taskText');
+    let taskSchedule = document.getElementById('taskSchedule');
+
+    todoList.push({ text: taskText.value, schedule: taskSchedule.value, completed: false });
+
+    // Clear the input fields
+    taskText.value = '';
+    taskSchedule.value = '';
+
+    saveTasks(); // Save the tasks after adding a new one
+    updateList();
+}
+
+function toggleComplete(index) {
+    todoList[index].completed = !todoList[index].completed;
+    saveTasks(); // Save the tasks after toggling completion
+    updateList();
+}
+
+function updateList() {
+    let listDiv = document.getElementById('todoList');
+    listDiv.innerHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        let taskDiv = document.createElement('div');
+
+        let checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.checked = todoList[i].completed;
+        checkBox.onchange = function() { toggleComplete(i); };
+        taskDiv.appendChild(checkBox);
+
+        let taskText = document.createElement('span');
+        taskText.innerText = todoList[i].text + ' [Schedule: ' + todoList[i].schedule + ']';
+        if (todoList[i].completed) {
+            taskText.className = 'completed';
+        }
+        taskDiv.appendChild(taskText);
+        
+        // Create a delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = function() { deleteTask(i); };
+        taskDiv.appendChild(deleteButton);
+
+        listDiv.appendChild(taskDiv);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadTasks(); // Load tasks when the document is loaded
+    document.getElementById('addTaskButton').addEventListener('click', addTask);
+});
